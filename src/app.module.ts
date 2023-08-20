b63f0b3afec2955a772418366b1591d925e92c33
HosamAlden <hosam98j@gmail.com>
2023-08-20 15:24:24 +0200

    edit env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,14 @@ import { MessagesGateway } from './messages/messages.gateway';
 import { MessagesModule } from './messages/messages.module';
 import { MessagesController } from './messages/messages.controller';
 import { MessagesService } from './messages/messages.service';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule,MongooseModule.forRoot("mongodb://localhost:27017/ChatApp"), MessagesModule, ConfigModule.forRoot({isGlobal: true})],
+  imports: [AuthModule,MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory:async (configService: ConfigService) => ({uri: configService.get("MONGODB_URI")})
+  }), MessagesModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AuthController, MessagesController],
   providers: [AuthService, MessagesGateway, MessagesService],
 })
