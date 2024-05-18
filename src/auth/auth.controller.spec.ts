587cc8cd34bb6f453b32a695f49bf538564202af
HosamAlden <hosam98j@gmail.com>
2024-05-18 15:12:36 +0200

    edit

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -6,7 +6,7 @@ import { AuthModule } from './auth.module';
 describe('AuthController', () => {
   let authController: AuthController;
   let authService: AuthService;
-
+  // change
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
