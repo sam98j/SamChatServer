1abdffbdf38e9b6abf8b14ec9dba3243462e0be0
HosamAlden <hosam98j@gmail.com>
2024-06-14 10:16:45 +0200

    edit cors settings

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://chat.samapps.xyz'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
